c73a003bc72ee81f64da5ccefb5f65352ec951d7
Solve4Theta <117078451+Solve4Theta@users.noreply.github.com>
2023-03-29 22:40:32 +0300

    Client\

---
+++ SSL_Handler.h
@@ -0,0 +1,20 @@
+#ifndef SSL_HANDLER_H
+#define SSL_HANDLER_H
+
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define FAIL -1
+
+SSL_CTX* InitCTX(void);
+void ShowCerts(SSL* ssl);
+
+#endif
