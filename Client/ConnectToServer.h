989a395adeab7e45b0025c1ccbcc77128234c9c2
Solve4Theta <117078451+Solve4Theta@users.noreply.github.com>
2023-03-29 22:51:12 +0300

    Add files via upload

---
+++ Client/ConnectToServer.h
@@ -0,0 +1,17 @@
+#ifndef CONNECT_TO_SERVER_H
+#define CONNECT_TO_SERVER_H
+
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+int ConnectServer();
+
+#endif
