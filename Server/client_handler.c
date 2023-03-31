9c5a39cbd1f82841b6e198092fb061a9a6d8d5bf
Tarmo Suvi <117308624+CharBehave@users.noreply.github.com>
2023-03-31 17:13:07 +0300

    Added linnked List
    Functions to handle linked list and sending messages to all clients

--- Server/client_handler.c
+++ Server/client_handler.c
@@ -18,6 +18,8 @@ void* client_handler(void *vargp)
 	char buf[1024] = {0};
 	int bytes;
 	
+	char* pBuf = buf;
+	
 	SSL *ssl = ((SSL *)vargp);
 	
 	
@@ -28,6 +30,8 @@ void* client_handler(void *vargp)
 		
 		printf("Client message: %s\n", buf);
 		
+		SendMessageToClients(pBuf);
+		
 		if (SSL_write(ssl, buf, strlen(buf)) > 0)
 		{
 			printf("Sent client message: %s\n", buf);
@@ -37,4 +41,84 @@ void* client_handler(void *vargp)
 	}
 	
 	return NULL;
+}
+
+
+void InsertClient(SSL *ssl, int id)
+{
+	
+	struct node *link = (struct node*) malloc(sizeof(struct node));
+	
+	link->client = ssl;
+	link->id = id;
+	
+	
+	link->pNext = head;
+	
+	head = link; 
+	
+	
+}
+
+
+
+struct node* DeleteClient(int ClientId)
+{
+	
+	struct node* current = head;
+	struct node* previous = NULL;
+	
+	if(head == NULL)
+	{
+		puts("Critical error! Client Delete NULL");
+		return NULL;
+	}
+	
+	while (current->id != ClientId)
+	{
+		if(current->pNext == NULL)
+		{
+			puts("Critical error! Client with this id does not exist!");
+			return NULL;
+		}
+		else
+		{
+			
+			previous = current;
+			
+			current = current->pNext;
+			
+		}
+		
+		
+		if (current == head)
+		{
+			head = head->pNext;
+		}
+		else
+		{
+			previous->pNext = current->pNext;
+		}
+		
+	}
+	
+	return current;
+}
+
+
+void SendMessageToClients(char* msg)
+{
+	struct node* ptr = head;
+	
+	while(ptr != NULL)
+	{
+		printf("Sending Client %d : %s", ptr->id, msg);
+		
+		SSL_write(ptr->client, msg, strlen(msg));
+		ptr = ptr->pNext;
+	}
+	
+	printf("Message '%s' sent to all clients\n", msg);
+	
+	
 }
