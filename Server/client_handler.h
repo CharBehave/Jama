96bc7fde1d9dc6bf023b88d8e8e2e79a7974f878
Tarmo Suvi <117308624+CharBehave@users.noreply.github.com>
2023-03-31 17:08:13 +0300

    Added linked list prototypes
    Linked list capability to handle all connected clients and send received messages to all of them.

--- Server/client_handler.h
+++ Server/client_handler.h
@@ -1,7 +1,32 @@
 #ifndef CLIENT_HANDLER_H
 #define CLIENT_HANDLER_H
 
+#include "server.h"
+#include "socket_handler.h"
+#include "SSL_handler.h"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "client_handler.h"
+
 void* client_handler(void *vargp);
+
+
+typedef struct node {
+	SSL *client;
+	int id;
+	struct node *pNext;
+	
+} clients_t;
+
+
+void InsertClient(SSL *ssl, int id);
+
+struct node* DeleteClient(int id);
+
+void SendMessageToClients(char* msg);
 
 
 #endif
