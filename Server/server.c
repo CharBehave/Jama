dd7b6b43eb94f55b2ffa7792d36b56d9880e1327
Tarmo Suvi <117308624+CharBehave@users.noreply.github.com>
2023-04-02 16:30:11 +0300

    Add files via upload

--- Server/server.c
+++ Server/server.c
@@ -8,72 +8,83 @@
 #include <pthread.h>
 #include "client_handler.h"
 
+
+
+
 int main(void)
 {
 	int socket;
 	SSL_CTX *ctx; //Declaration of SSL_CTX object
+	
 	
 	socket = BindAndListenToPort(8888);
 	
 	ctx = create_context(); //create SSL_CTX object
 	
 	Configure_context(ctx); //add cert and key to SSL_CTX object
+	SSL *ssl;
 	
-	int clientId;
+	puts("SSL context configured successfully, server is up and running!");
+	
+	int connectedClientId = 0;
 	
 	while (1)
 	{
 		struct sockaddr_in addr;
 		unsigned int len = sizeof(addr);
-		SSL *ssl;
-		const char reply[] = "test\n";
+		
 		//thread id
 		pthread_t tid; 
 		
 		
-		puts("Waiting for SSL/TSL connection!");
+		puts("Ready to accept next connection");
 		
-		
+
 		int client = accept(socket, (struct sockaddr*)&addr, &len);
 		
 
 		
 		if (client < 0)
 		{
-			puts("unable to accept client! exiting");
-			exit(EXIT_FAILURE);
+			puts("unable to accept client!");
+		}
+		else
+		{
+			
+			ssl = SSL_new(ctx); //create new SSL structure for TLS/SSL connection
+			SSL_set_fd(ssl, client); //set client as the IO facility for encryption
+			
 		}
 		
-		ssl = SSL_new(ctx); //create new SSL structure for TLS/SSL connection
-		
-		SSL_set_fd(ssl, client); //set client as the IO facility for encryption
+
 		
 		if (SSL_accept(ssl) <= 0)
 		{
-			puts("Error accepting TLS/SSL handshake connection!");
+			puts("Error accepting TLS/SSL handshake!");
 		}
 		else
 		{
 			puts("Handshake accepted!");
-			clientId++;
+			connectedClientId++; 
 			
-			InsertClient(ssl, clientId);
-			
+			InsertClient(ssl, connectedClientId, client);
 			
 			pthread_create(&tid, NULL, client_handler, ssl);
-			
-			
-			
 		}
 		
 		
 		
-		//SSL_shutdown(ssl);
-		//SSL_free(ssl);
-		//close(client);
+		
 	}
+	//Should never actually reach here, if it does without CTRL + C command,
+	//then it's between you and god
+	printf("Closing server!\n");
 	
+	SSL_free(ssl);
 	close(socket);
 	SSL_CTX_free(ctx);
 }
+
+
+
 
