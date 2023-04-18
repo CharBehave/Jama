a177e5451c39a10bc5a4686a73fd48ca4ff942d1
Solve4Theta <117078451+Solve4Theta@users.noreply.github.com>
2023-04-18 22:01:23 +0300

    Add files via upload

---
+++ Basic_Client/client.h
@@ -0,0 +1,22 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <pthread.h>
+
+#define FAIL -1
+
+void * ReadFunc(void * ssl);
+void * WriteFunc(void * ssl);
+
+#endif
